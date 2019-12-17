3e207afcebe22026967f158716ade67b6b414ada
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2019-12-17 01:26:54 -0500

    change server to actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,35 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use]
-extern crate rocket;
-
+use actix_files::NamedFile;
+use actix_web::{web, App, HttpRequest, HttpServer, Result};
 use std::path::{Path, PathBuf};
 
-use rocket::response::NamedFile;
-
-#[get("/")]
-fn index() -> Option<NamedFile> {
-    NamedFile::open(Path::new("public/index.html")).ok()
+fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open(Path::new("./public/index.html"))?)
 }
 
-#[get("/game.html")]
-fn game() -> Option<NamedFile> {
-    NamedFile::open(Path::new("public/game.html")).ok()
+fn favicon() -> Result<NamedFile> {
+    Ok(NamedFile::open(Path::new("./public/static/favicon.ico"))?)
 }
 
-#[catch(404)]
-fn not_found() -> Option<NamedFile> {
-    NamedFile::open(Path::new("public/404.html")).ok()
-}
-
-#[get("/<file..>", rank = 2)]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("dist/").join(file)).ok()
-}
-
-#[get("/static/<file..>", rank = 1)]
-fn static_files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("public/static/").join(file)).ok()
-}
-
-#[get("/favicon.ico", rank = 0)]
-fn favicon() -> Option<NamedFile> {
-    NamedFile::open(Path::new("public/static/favicon.ico")).ok()
+fn files(req: HttpRequest) -> Result<NamedFile> {
+    let file: PathBuf = req.match_info().query("filename").parse().unwrap();
+    if let Ok(file) = NamedFile::open(Path::new("./public").join(&file)) {
+        Ok(file)
+    } else {
+        Ok(NamedFile::open(Path::new("./dist").join(file))?)
+    }
 }
 
 fn main() {
-    rocket::ignite()
-        .register(catchers![not_found])
-        .mount("/", routes![index, game, files, static_files, favicon])
-        .launch();
+    println!("The server has been started!: http://0.0.0.0:7878 \n");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/favicon.ico", web::get().to(favicon))
+            .route("/{filename:.*}", web::get().to(files))
+    })
+    .bind("0.0.0.0:7878")
+    .unwrap()
+    .run()
+    .unwrap();
 }
